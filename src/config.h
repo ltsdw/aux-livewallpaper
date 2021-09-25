fbc24ad5ff690498c61dfd99b9021297f7df9d06
ltsdw <44977415+ltsdw@users.noreply.github.com>
2021-09-25 14:49:51 -0300

    typo

--- src/config.h
+++ src/config.h
@@ -6,7 +6,7 @@ char media[] = "demon-mask-pixel-720p-18fps.mp4";
 // compositor[0] = command
 // compositor[@] = args
 // the last argument has to be NULL
-// char* compositor[] = {"/usr/bin/picom", "/home/user/.config/picom/picom.conf", NULL};
+// char* compositor[] = {"/usr/bin/picom", "--config", "/home/user/.config/picom/picom.conf", NULL};
 char* compositor[] = {NULL};
 
 #endif
